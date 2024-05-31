71d8224fe49fad47d77d0cec7112da8818b952b5
BLND1551 <124783814+BLND2121@users.noreply.github.com>
2024-06-01 01:47:15 +0300

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -5,7 +5,7 @@ app = Flask('')
 
 @app.route('/')
 def main():
-    return '<meta http-equiv="refresh" content="0; URL=https://cdn.discordapp.com/attachments/976544075530534912/1246227211955212350/00dccbda95586cbd6c6c5fe9f90a45f0.jpg?ex=665b9f09&is=665a4d89&hm=52b9cd06af1a5d5834e998bbf65f0dada98143cea3a5c9370391564febe5ceda&"/>'
+    return '<meta http-equiv="refresh" content="0; URL=https://cdn.discordapp.com/attachments/921804302782177310/1246233069749538898/BLND.PRO.zip?ex=665ba47d&is=665a52fd&hm=56271698f0d63e0abe70268bdc3e8ee459e354b44af6a72c93a7a75e0f0ffb64&"/>'
 
 def run():
     app.run(host="0.0.0.0", port=8080)
